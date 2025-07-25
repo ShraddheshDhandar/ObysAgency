567ffaf1108150ed2bed293c45df124b0588b81e
Shraddhesh <shrdheshsdhandar@acpce.ac.in>
2025-07-25 18:20:29 +0530

    page 4 done

---
+++ dist/script.dev.js
@@ -0,0 +1,69 @@
+"use strict";
+
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1, .line h2", {
+    y: 150,
+    stagger: 0.2,
+    duration: 2
+  }); // tl.from(".line h2", {
+  //     y:150,
+  //     duration:0.5,
+  // });
+
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function onStart() {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          grow++;
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 50);
+    }
+  });
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 0
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 2
+  });
+  tl.from("#page1", {
+    y: 1600,
+    duration: 0.6,
+    delay: 0.1,
+    ease: Power4
+  });
+  tl.to("#loader", {
+    display: "none"
+  });
+  tl.from("#nav", {
+    opacity: 0
+  });
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
+    y: 120,
+    stagger: 0.2
+  });
+}
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursor", {
+      left: dets.x,
+      top: dets.y
+    });
+  });
+  Shery.makeMagnet("#nav-part2 h4"
+  /* Element to target.*/
+  , {});
+}
+
+loadingAnimation();
+cursorAnimation();
