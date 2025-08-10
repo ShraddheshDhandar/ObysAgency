1f62352973db9ae13f590982cac1085be522d6bd
Shraddhesh <shrdheshsdhandar@acpce.ac.in>
2025-08-10 13:20:58 +0530

    footer effect

--- script.js
+++ script.js
@@ -152,33 +152,96 @@ Shery.imageEffect(".image-div", {
 });
 }
 
-document.addEventListener("mouse,move", function(dets){
-    gsap.to("flag",{
-        x:dets.x,
-        y:dets.y
+function flagAnimation() {
+
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#flag", {
+      x: dets.x,
+      y: dets.y
     })
-})
+  })
+  document.querySelector("#hero3").addEventListener("mouseenter", function () {
+    gsap.to("#flag", {
+      opacity: 1
+    })
+  })
+  document.querySelector("#hero3").addEventListener("mouseleave", function () {
+    gsap.to("#flag", {
+      opacity: 0
+    })
+  })
 
-document.querySelector("#hero3").addEventListener("mouseenter",
-    function(){
-        gsap.to("#flag",{
-            opacity:1
+}
+
+function footerAnimation() {
+
+  // var clutter = ""
+  // var clutter2 = ""
+  // document.querySelector("#footer h1").textContent.split("").forEach(function (elem) {
+  //   clutter += `<span>${elem}</span>`
+  // })
+  // document.querySelector("#footer h1").innerHTML = clutter
+  // document.querySelector("#footer h2").textContent.split("").forEach(function (elem) {
+  //   clutter2 += `<span>${elem}</span>`
+  // })
+  // document.querySelector("#footer h2").innerHTML = clutter2
+
+
+  // document.querySelector("#footer h1").addEventListener("mouseenter", function () {
+  //   gsap.to("#footer h1 span", {
+  //     opacity: 0,
+  //     stagger: 0.05
+  //   })
+  //   gsap.to("#footer h2 span", {
+  //     delay: 0.35,
+  //     opacity: 1,
+  //     stagger: 0.1
+  //   })
+  // })
+  // document.querySelector("#footer h1").addEventListener("mouseleave", function () {
+  //   gsap.to("#footer h1 span", {
+  //     opacity: 1,
+  //     stagger: 0.1,
+  //     delay: 0.35,
+
+  //   })
+  //   gsap.to("#footer h2 span", {
+  //     opacity: 0,
+  //     stagger: 0.05
+  //   })
+  // })
+
+
+  $(document).ready(function(){
+    $('.tlt').textillate({
+        loop: true,
+        minDisplayTime: 2000,
+        initialDelay: 0,
+        in: {
+            effect: 'fadeIn',
+            delayScale: 1.5,
+            delay: 50,
+            sync: false
+        },
+        out: {
+            effect: 'fadeOut',
+            delayScale: 1.5,
+            delay: 50,
+            sync: false
         }
-        )
-    }
-)
+    });
+});
 
-document.querySelector("#hero3").addEventListener("mouseleave",
-    function(){
-        gsap.to("#flag",{
-            opacity:0
-        }
-        )
-    }
-)
-
+}
 
 loadingAnimation()
 cursorAnimation()
-locomotiveAnimation()
+// locomotiveAnimation()
 sheryAnimation()
+flagAnimation()
+// footerAnimation()
+
+window.addEventListener('load', () => {
+    locomotiveAnimation();
+    footerTextAnimation();
+});
